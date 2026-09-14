4cd2d299c3cfe0cb656f9010f5c20a8f522d6df1
Kryzn <haver3101@gmail.com>
2026-09-14 20:00:43 -0300

    Update db.js

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'SUA_SENHA_AQUI',
+  password: '7433',
   database: 'quiz_app',
   waitForConnections: true,
   connectionLimit: 10
